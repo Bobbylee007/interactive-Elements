eb04163fe616c47d38f7a7116a082fcf58636f18
Bobbylee007 <bobbyleeaustine@gmail.com>
2025-10-13 08:29:38 +0100

    toggle sun and moon

--- buttonComponet/bouncy-3d-button/index.js
+++ buttonComponet/bouncy-3d-button/index.js
@@ -1,7 +1,30 @@
+// Setup/logic
 
-const theme = document.querySelector(".btn-theme");
+//toggle Light and  Dark mode
+// let theme = document.getElementsByTagName("button")[1];
+const root = document.querySelector(":root");
+const img = document.querySelector("img");
 
+let darkMode = false;
 
-theme.addEventListenerr('click', (e)=>{
-    
-})
+// theme.addEventListener("click", (e) => {
+//   if (theme.e.target === "IMG") {
+//     root.style.setProperty("--color-bg", "hsl(0, 3%, 7%)");
+//     root.style.setProperty(" --color-container", "hsla(0, 0%, 8%, 1.00)");
+//   }
+// });
+
+img.addEventListener("click", function changeTheme() {
+  if (!darkMode) {
+    root.style.setProperty("--color-bg", "hsla(0, 0%, 8%, 1.00");
+    root.style.setProperty("--color-container", "hsla(0, 1%, 18%, 1.00)");
+    root.style.setProperty("--color-text", "hsla(0, 9%, 91%, 1.00)");
+    img.setAttribute("src", "./asset/garden_sun-fill-16.svg");
+  } else {
+    root.style.setProperty("--color-bg", " hsl(0, 0%, 100)");
+    root.style.setProperty("--color-container", "hsl(0, 0%, 95%)");
+    root.style.setProperty("--color-text", "hsl(0, 2%, 12%)");
+    img.setAttribute("src", "./asset/garden_moon-fill-16.svg");
+  }
+  darkMode = !darkMode;
+});
