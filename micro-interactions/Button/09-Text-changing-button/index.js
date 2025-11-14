5ada55dfcc02edb7670e9801bd6222376fa4180d
Bobbylee007 <bobbyleeaustine@gmail.com>
2025-11-14 10:41:31 +0100

    Text change

---
+++ micro-interactions/Button/09-Text-changing-button/index.js
@@ -0,0 +1,62 @@
+// Setup/logic
+
+/*- create button ui with html, css
+  - give the ui a default style 
+  - create a clicking efftect with active State (using transform, tranlate)
+  - or js to give click interaction by changing active state class 
+  - add click sound with js using var or let to add instanse of Audio() object
+*/
+
+//toggle Light and  Dark mode
+const root = document.querySelector(":root");
+const btn = document.querySelector(".btn-theme");
+const img = document.querySelector("img");
+
+let darkMode = false;
+
+btn.addEventListener("click", () => {
+  if (!darkMode) {
+    root.style.setProperty("--color-bg", "hsla(0, 0%, 8%, 1.00");
+    root.style.setProperty("--color-container", "hsla(0, 1%, 18%, 1.00)");
+    root.style.setProperty("--color-text", "hsla(0, 9%, 91%, 1.00)");
+    root.style.setProperty("--clr-brd", "hsl(226, 71%, 52%)");
+    img.setAttribute("src", "./asset/garden_sun-fill-16.svg");
+  } else {
+    root.style.setProperty("--color-bg", " hsl(0, 0%, 100)");
+    root.style.setProperty("--color-container", "hsl(0, 0%, 95%)");
+    root.style.setProperty("--color-text", "hsl(0, 2%, 12%)");
+    img.setAttribute("src", "./asset/garden_moon-fill-16.svg");
+  }
+  darkMode = !darkMode;
+});
+
+//Ripple Effect
+
+/*- create button ui with html, css
+  - give the ui a default style 
+  - JS: select the DOM and handle the event 
+  - when click create an icon or image tag and setAttribut
+    to include icon src dir
+  - set text content to empty string
+  setTimeout to add textContent to Done
+  - remove eventlistener
+
+*/
+
+let btnId = document.querySelector(".btn");
+
+btnId.addEventListener("click", () => {
+  let iconImg = document.createElement("img");
+  btnId.textContent = "";
+  iconImg.setAttribute("src", "./asset/notch.svg");
+  btnId.append(iconImg);
+  iconImg.classList.add("notch");
+
+  setTimeout(() => {
+    btnId.textContent="Done"
+  }, 2000);
+});
+
+setTimeout(()=>{
+  btnId.removeEventListener('click')
+}, 1000)
